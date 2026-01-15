453000b6a0aae440bbb90fe0a0fa7b92d8d24408
mohitcodecpp-cpu <mohitcode.cpp@gmail.com>
2026-01-15 19:01:39 +0530

    first code

--- Basics/week1.cpp
+++ Basics/week1.cpp
@@ -1,24 +1,17 @@
 #include <iostream>
 using namespace std;
-int main (){
-    for (int n=2; n<=100 ; n++){
-        bool isPrime =true;
-        for(int i =2 ; i<= n-1 ; i++){
-            if(n%i ==0){
-                isPrime = false;
-                break;
-            }
-        }if(isPrime==true){
-            cout << n <<" ";
-        }
-    }
-    
-    
-cout<< endl;
-return 0;
+int main () { 
+    for(int m =1; m<=5 ; m++){
+        for (int n=1 ; n<=m; n++){cout<<n <<" ";}
+        cout<<endl;
 
 }
+return 0;
 
+
+  
+
+}
 
 
 
